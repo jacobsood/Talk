c1391ddc05d183297195aa8278380711f57585cc
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-19 22:18:24 +0000

    Added tests related to the engine being used. Updated language and voice tests. Added priority tests

--- tests/polly/connection.test.js
+++ tests/polly/connection.test.js
@@ -24,10 +24,11 @@ describe('Check if language settings are correct and available by Polly', () => 
         settings.language = 'english';
         expect(settings.language).toBe('en-US');
     });
-    it('Language code for unknown language must be en-US', () => {
+    it('Language code for unknown language must not change', () => {
         var settings = pollyConnection.setupPolly();
+        let lang = settings.language;
         settings.language = 'xhosa';
-        expect(settings.language).toBe('en-US');
+        expect(settings.language).toBe(lang);
     });
     it('Language code for empty string must not change', () => {
         var settings = pollyConnection.setupPolly();
@@ -89,7 +90,7 @@ describe('Check if voice chosen are valid', () => {
         var settings = pollyConnection.setupPolly();
         expect(settings.VoiceId).toBe('Joanna');
     });
-    it('Changing just the voice id should also change to it\'s corresponding language code', () => {
+    it('Changing just the voice id should also change to its corresponding language code', () => {
         var settings = pollyConnection.setupPolly();
         settings.voice = 'Zhiyu';
         expect(settings.voice).toBe('Zhiyu');
@@ -141,9 +142,97 @@ describe('Voice should change to its appropriate default when changing the langu
         settings.language = 'brazillian portuguese';
         expect(settings.voice).toBe('Camila');
     });
+    it('Language set to turkish and voice set to Mathieu. Language should automatically change to fr-FR', ()=> {
+        var settings = pollyConnection.setupPolly();
+        settings.language = 'turkish';
+        expect(settings.language).toBe('tr-TR');
+        settings.voice = 'Mathieu';
+        expect(settings.language).toBe('fr-FR');
+        expect(settings.voice).toBe('Mathieu');
+    });
+    it('Setting language to hindi and setting voice to Aditi. Changing language to en-IN should not change voice - bilingual', ()=> {
+        var settings = pollyConnection.setupPolly();
+        settings.language = 'hindi';
+        settings.voice = 'Aditi';
+        expect(settings.voice).toBe('Aditi');
+        expect(settings.language).toBe('hi-IN');
+        settings.language = 'indian english';
+        expect(settings.language).toBe('en-IN');
+        expect(settings.voice).toBe('Aditi');
+    });
 });
 
-describe('Checking if priorities hold with polly configuration', () => {
+describe('Testing if the engine is correctly chosen with the appropriate voice and language', () =>{
+    it('Changing engine to neural should ensure it is available with the default settings', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.engine = 'neural';
+        expect(settings.language).toBe('en-US');
+        expect(settings.voice).toBe('Joanna');
+        expect(settings.engine).toBe('neural');
+    });
+    it('Changing language to Hindi. Default voice of hindi does not allow neural, and hence engine change to neural should not be allowed', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.engine = 'neural';
+        settings.language = 'hindi';
+        expect(settings.language).toBe('hi-IN');
+        expect(settings.engine).toBe('standard');
+    });
+    it('Engine must automatically change to neural when choosing Kevin\'s voice, as it does not support standard engine', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.voice = 'Kevin';
+        expect(settings.language).toBe('en-US');
+        expect(settings.voice).toBe('Kevin');
+        expect(settings.engine).toBe('neural');
+    });
+    it('Changing Kevin\'s voice to standard should not work', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.voice = 'Kevin';
+        expect(settings.voice).toBe('Kevin');
+        settings.engine = 'standard';
+        expect(settings.engine).toBe('neural');
+    });
+    it('Changing from neural to standard', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.engine = 'neural';
+        expect(settings.engine).toBe('neural');
+        settings.engine = 'standard';
+        expect(settings.engine).toBe('standard');
+    });
+    it('Enabling neural for a supported voice and then changing the voice to another supported voice should still be neural', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.engine = 'neural';
+        expect(settings.engine).toBe('neural');
+        settings.voice = 'Kimberly';
+        expect(settings.engine).toBe('neural');
+    });
+    it('Changing to a voice that does not support neural should change its engine to standard', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.language = 'brazillian portuguese';
+        settings.engine = 'neural';
+        expect(settings.voice).toBe('Camila');
+        expect(settings.engine).toBe('neural');
+        settings.voice = 'Vitoria';
+        expect(settings.engine).toBe('standard');
+        expect(settings.voice).toBe('Vitoria');
+    });
+    it('Changing from en-GB with neural engine to en-US. Engine should remain neural as it is supported by its default voice', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.language = 'british english';
+        settings.engine = 'neural';
+        expect(settings.engine).toBe('neural');
+        settings.language = 'american english';
+        expect(settings.engine).toBe('neural');
+    });
+    it('Changing from a voice and language with neural engine to a language with no neural support on its default voice should have the standard engine', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.engine = 'neural';
+        expect(settings.engine).toBe('neural');
+        settings.language = 'korean';
+        expect(settings.engine).toBe('standard');
+    });
+});
+
+describe('Checking if priorities hold with polly configuration. Priorities: language = person > engine', () => {
     it('Changing voice before language should not change voiceID to default of the language, if voice exists for that language',() => {
         var settings = pollyConnection.setupPolly();
         settings.voice = 'Lucia';
@@ -155,5 +244,16 @@ describe('Checking if priorities hold with polly configuration', () => {
         settings.voice = 'Carmen';
         settings.language = 'spanish';
         expect(settings.voice).toBe('Conchita');
+    });
+    it('Changing person after enabling neural should disable neural if it is not compatible with the person', () => {
+        var settings = pollyConnection.setupPolly();
+        settings.language = 'brazillian portuguese';
+        expect(settings.voice).toBe('Camila');
+        settings.engine = 'neural';
+        expect(settings.engine).toBe('neural');
+        expect(settings.voice).toBe('Camila');
+        settings.voice = "Ricardo";
+        expect(settings.engine).toBe('standard');
+        expect(settings.voice).toBe('Ricardo');
     });
 });
